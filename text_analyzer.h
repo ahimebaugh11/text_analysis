642b65538126a2a72ae0fd88379d626fdd73ad73
ahimebaugh11 <37554783+ahimebaugh11@users.noreply.github.com>
2019-12-05 11:30:43 -0600

    added header file

---
+++ text_analyzer.h
@@ -0,0 +1,24 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Word { 
+    char txt[50]; 
+    struct Word *next; 
+} Word;
+
+void print(struct Word head){
+    int count = 0;
+    struct Word word = head;
+    while(count<10){
+        printf("%s", word.txt);
+        count++;
+        word = *head.next;
+    }
+};
+
+
+#endif
